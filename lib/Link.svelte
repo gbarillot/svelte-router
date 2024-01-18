0c9a1add04288a5987a8716c00b2ee9b6b496e16
Guillaume Barillot <guillaume@Ordinateur-portable-de-Guillaume.local>
2024-01-18 19:26:11 +0100

    Implement named routes

--- lib/Link.svelte
+++ lib/Link.svelte
@@ -1,9 +1,23 @@
 <script>
-  import { link } from './route'
+  import { link, router } from './route'
 
-  export let href
-  export let activeClass = 'active'
-  export let exactActiveClass = 'exact-active'
+  export let to;
+  export let params;
+  export let path;
+  export let query;
+  
+  export let attrs;
+  export let data;
+ 
+  export const htmlAttrs = attrs === undefined ? {} : attrs
+  export const dataAttrs = data === undefined ? {} : toDataAttrs(data)   
+
+  function toDataAttrs(data) {
+    return Object.keys(data).reduce((acc, key) => {
+      acc[`data-${key}`] = data[key];
+      return acc;
+    }, {});
+  }
 </script>
 
-<a use:link={{activeClass, exactActiveClass}} {href} {...$$restProps}><slot></slot></a>
+<a use:link={{to, params, attrs, path, query}} href='#/' {...htmlAttrs} {...dataAttrs}><slot></slot></a> 
